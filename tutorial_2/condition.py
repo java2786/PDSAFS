29d7dbe2abdf11347c24dc76cd9b6e1df56bdd14
Arun Kumar <java.kumar.arun@gmail.com>
2026-05-01 11:16:01 +0530

    30May

---
+++ tutorial_2/condition.py
@@ -0,0 +1,24 @@
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+a = 23
+b = 23
+
+if(a>b):
+    print("Larger number is",a)
+else:
+    print("Larger number is",b)
+
+print("end of code")
+
+
+
+
+
+
+
+
+
+
+
+
+
