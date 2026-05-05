0e37499bb83cf3c2b1592287cb962acc414a617a
Arun Kumar <java.kumar.arun@gmail.com>
2026-05-05 12:00:17 +0530

    6may

---
+++ tutorial_2/license.py
@@ -0,0 +1,12 @@
+age = 20
+
+# if(age>=18 and age<=80):
+#     print("************ VALID ***************")
+
+# else:
+#     print("not valid")
+
+if(age<18 or age>80):
+    print("not valid")
+else:
+    print("************ VALID ***************")
---
+++ tutorial_2/odd_even.py
@@ -0,0 +1,8 @@
+# Input a number and print whether it's even or odd (no if/else, just logic with %).
+
+num = 4
+
+if(num%2==0):
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
